3335b9144317046f973c76c2d8f6b130369b2194
Paul Fornage <36117326+paulwrath1223@users.noreply.github.com>
2024-08-23 21:16:41 -0700

    temp BT serial bridge to see how the hell that damned app does it

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 #include "Arduino.h"
 
 
-BluetoothSerial SerialBT;
-#define ELM_PORT   SerialBT
+BluetoothSerial SerialBTElm;
+BluetoothSerial SerialBTSlave;
+#define ELM_PORT   SerialBTElm
 #define DEBUG_PORT Serial
+#define PROXY_PORT SerialBTSlave
 
 
 ELM327 myELM327;
@@ -13,6 +15,7 @@ ELM327 myELM327;
 
 uint32_t rpm = 0;
 
+int temp_in;
 
 void setup()
 {
@@ -25,23 +28,23 @@ void setup()
     //SerialBT.setPin("1234");
     ELM_PORT.begin("ArduHUD", true);
 
-    BTScanResults* results = ELM_PORT.discover(60000);
-    const int num_results = results->getCount();
-    BTAdvertisedDevice* temp_device;
-    String device_name;
-    String device_mac;
-
-    for(int i = 0; i < num_results; i++){
-        temp_device = results->getDevice(i);
-        device_name = temp_device->getName().c_str();
-        device_mac = temp_device->getAddress().toString().c_str();
-
-        DEBUG_PORT.print("found device {");
-        DEBUG_PORT.print(device_name);
-        DEBUG_PORT.print("} with address {");
-        DEBUG_PORT.print(device_mac);
-        DEBUG_PORT.println("}.");
-    }
+//    BTScanResults* results = ELM_PORT.discover(60000);
+//    const int num_results = results->getCount();
+//    BTAdvertisedDevice* temp_device;
+//    String device_name;
+//    String device_mac;
+//
+//    for(int i = 0; i < num_results; i++){
+//        temp_device = results->getDevice(i);
+//        device_name = temp_device->getName().c_str();
+//        device_mac = temp_device->getAddress().toString().c_str();
+//
+//        DEBUG_PORT.print("found device {");
+//        DEBUG_PORT.print(device_name);
+//        DEBUG_PORT.print("} with address {");
+//        DEBUG_PORT.print(device_mac);
+//        DEBUG_PORT.println("}.");
+//    }
 
     if (!ELM_PORT.connect("VEEPEAK"))
     {
@@ -55,21 +58,34 @@ void setup()
         while (1);
     }
 
-    Serial.println("Connected to ELM327");
+    Serial.println("Connected to ELM327\nInitiating Slave");
 
-
+    PROXY_PORT.begin("OBD2", false);
 }
 
 
 void loop()
 {
-    float tempRPM = myELM327.rpm();
+//    float tempRPM = myELM327.rpm();
+//
+//    if (myELM327.nb_rx_state == ELM_SUCCESS)
+//    {
+//        rpm = (uint32_t)tempRPM;
+//        Serial.print("RPM: "); Serial.println(rpm);
+//    }
+//    else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
+//        myELM327.printError();
 
-    if (myELM327.nb_rx_state == ELM_SUCCESS)
-    {
-        rpm = (uint32_t)tempRPM;
-        Serial.print("RPM: "); Serial.println(rpm);
+    if (ELM_PORT.available()) {
+        temp_in = ELM_PORT.read();
+        PROXY_PORT.write(temp_in);
+        DEBUG_PORT.print("Elm -> App: ");
+        DEBUG_PORT.println(temp_in);
     }
-    else if (myELM327.nb_rx_state != ELM_GETTING_MSG)
-        myELM327.printError();
+    if (PROXY_PORT.available()) {
+        temp_in = PROXY_PORT.read();
+        ELM_PORT.write(temp_in);
+        DEBUG_PORT.print("App -> Elm: ");
+        DEBUG_PORT.println(temp_in);
+    }
 }
